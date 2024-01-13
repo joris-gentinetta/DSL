f67417e22d9cf6c7bc26aee25a533373a9985882
jg_mac <gentinetta.joris@gmail.com>
2024-01-13 18:22:34 +0100

    cleanup environment creation

---
+++ envs/create_env.sh
@@ -0,0 +1,4 @@
+conda env remove -n DSL -y
+conda env create -f environment.yml
+conda activate DSL || exit
+
--- envs/segment.sh
+++
@@ -1,4 +0,0 @@
-conda env remove -n segment -y
-conda env create -f segment_env.yml conda activate segment
-conda activate segment || exit
-
